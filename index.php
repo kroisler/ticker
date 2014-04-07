aebfb0ebf82023c17248723a2bdcc6b9b2393a21
kroisler <Florianraedler@web.de>
2014-04-07 14:10:52 +0200

    b

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Beschreibung der Seite</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="http://code.google.com/apis/gears/gears_init.js" type="text/javascript"></script>
+<script src="http://m5.androidapps.at/geo/js/geo.js" type="text/javascript" ></script>
+
+</head>
+<body>
+
+<div id="box">
+<div class="form">
+<center>
+<form method="POST" action="../i.php">
+<label class="heim">HEIM</label> - <label class="gast">GAST</label> <br><br>
+<input type="text" name="Heim" value="Heim" onFocus="if(this.value=='Heim') this.value=''" /> - <input type="text" name="Gast" value="Gast"  onFocus="if(this.value=='Gast') this.value=''" /><br><br>
+<input type="text" name="EMail" value="E-Mail" onFocus="if(this.value=='E-Mail') this.value=''" />
+<input type="date" name="Datum" value="Datum" onFocus="if(this.value=='Datum') this.value=''" />
+<br>
+<br><input type="submit" class="button" name="submitted" value="ANLEGEN"><br>
+
+</form>
+
+</center>
+<br>
+<center>
+
+</center>
+</div>
+</div>
+<div id="add">
+<img width="318px" height="50px" src="../img/fbibw.png" alt="logo" >
+
+</div>
+
+</body>
+</html>
+
+ 
+
+
+
